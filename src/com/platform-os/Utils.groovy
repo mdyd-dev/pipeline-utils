75248ce67982d0ec3bb1fb2691e9d285a638209e
godot <dgorzeba@gmail.com>
2019-03-04 22:54:27 +0100

    update Utils

--- src/com/platform-os/Utils.groovy
+++ src/com/platform-os/Utils.groovy
@@ -1,4 +1,7 @@
 package com.platform-os
 
 class Utils {
+  static def alert(message, channel='eng-builds') {
+    slackSend color: 'good', message: "${message} (<${env.BUILD_URL}console|Open>)", channel: "#${channel}"
+  }
 }
