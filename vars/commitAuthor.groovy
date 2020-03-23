9ff768b3f8b005de76c1fa1470478dda3d851c64
Ignacy Moryc <imoryc@gmail.com>
2020-03-23 15:15:34 +0100

    Use lookup table for names

--- vars/commitAuthor.groovy
+++ vars/commitAuthor.groovy
@@ -1,8 +1,21 @@
 #!/usr/bin/env groovy
 
 def call() {
-  sh(
+  def lookupNames = [:]
+  lookupNames["Ignacy Moryc"] = "@ignacy"
+  lookupNames["Maciej Krajowski-Kukiel"] = "@Slashek"
+  lookupNames["piotrze"] = "@piotrze"
+  lookupNames["godot"] = "@godot"
+
+  def name = sh(
     script: "git --no-pager show -s --format='%an'",
     returnStdout: true
   ).trim()
+
+
+  if (lookupNames.containsKey(name)) {
+    lookupNames.get(name)
+  } else {
+    name
+  }
 }
